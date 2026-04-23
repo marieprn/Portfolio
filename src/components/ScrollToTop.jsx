2855b8d5dfa8b3e77e1ac933f43e88b24df64d75
Marie Peron <marie.prn77@gmail.com>
2026-04-23 10:42:25 +0200

    Modification scrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,8 +5,8 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    window.scrollTo({ top: 0, behavior: 'instant' })
+  }, [pathname])
 
   return null;
 }
